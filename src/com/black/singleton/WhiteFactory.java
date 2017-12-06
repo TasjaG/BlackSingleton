ab47f6f712f343ff6826c0df2aa252665deabd79
Viktoria Romanenko <kvika@ipnet.ua>
2017-12-06 18:24:25 +0200

    Add annotation and toString method

--- src/com/black/singleton/WhiteFactory.java
+++ src/com/black/singleton/WhiteFactory.java
@@ -4,11 +4,19 @@ package com.black.singleton;
  * Created by Victoria on 05.12.2017.
  */
 public class WhiteFactory extends BaseFactory {
+
+    @Override
     Circle createCircle(Point p, int radius) {
         return new WhiteCircle(p, radius);
     }
 
+    @Override
     Triangle createTriangle(Point p1, Point p2, Point p3) {
         return new WhiteTriangle(p1, p2, p3);
+    }
+
+    @Override
+    public String toString() {
+        return "White Factory";
     }
 }
