39a10fa9a61a8d9dbbc9cc41fb887888e556727d
greegav <greegav@gmail.com>
2017-12-13 16:02:32 +0200

    fixed name

--- src/com/black/singleton/Triangle.java
+++ src/com/black/singleton/Triangle.java
@@ -4,14 +4,16 @@ public abstract class Triangle {
     private Point a;
     private Point b;
     private Point c;
+    private String name;
 
     public Triangle(Point a, Point b, Point c) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.name=getClass().getSimpleName();
     }
 
     public String toString() {
-        return this.getClass().getSimpleName();
+        return this.name;
     }
 }
